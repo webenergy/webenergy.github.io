d04abfa9abaae2dda4ad9088425ef579dca787c8
Zhang Guangda <zhangxiaoyu9350@gmail.com>
2013-01-09 18:39:40 +0800

    初始创建
    webenergy.github.com

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-webenergy.github.com
-====================
+# weframework website
 
-WeFramework
+这个分支用以开发We Framework对外说明文档
+
+
+文档地址：[We Framework Doc](http://inkless.github.com/weframework "We Framework Doc")
---
+++ js/main.js
@@ -0,0 +1,15 @@
+$we.require("core", function() {
+
+	$(function() {
+		$('.runnable').each(function() {
+			var code = this;
+			var button = '<div class="run" title="运行"></div>';
+			$(button).insertBefore(code).bind('click', function(){
+				$we.utils.exec($(code).find("code").text());
+			});
+		});
+
+		hljs.tabReplace = '    '; // 4 spaces
+	  	hljs.initHighlightingOnLoad();
+	});
+});
